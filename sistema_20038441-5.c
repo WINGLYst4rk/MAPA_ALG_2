c97face5c5c87f09efc9270ee9fcf15cc479e10d
WINGLYst4rk <wingly205@gmail.com>
2020-07-13 18:58:21 -0300

    correção do arquivo main.c para adequar ao jeito em que minha faculdade pediu para ser enviado

---
+++ sistema_20038441-5.c
@@ -0,0 +1,15 @@
+/* arquivo para resolver o problema proposto para a matéria de algoritmos e logiga de programação 2
+incialização das bibliotecas necessarias para a criação do código a ser desenvolvido.
+@author: Wingly Silva Rodrigues
+RA: 20038441-5
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main() {
+	
+	
+}
