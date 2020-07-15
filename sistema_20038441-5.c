2f46908c711f53241acdf4f1137363a61e5b803c
WINGLYst4rk <wingly205@gmail.com>
2020-07-15 20:52:03 -0300

    refatoramento do código com adição do arquivo

--- sistema_20038441-5.c
+++ sistema_20038441-5.c
@@ -1,48 +1,74 @@
-/* arquivo para resolver o problema proposto para a matéria de algoritmos e logiga de programação 2
-incialização das bibliotecas necessarias para a criação do código a ser desenvolvido.
-@author: Wingly Silva Rodrigues
-RA: 20038441-5
-*/
+// arquivo para resolver o problema proposto para a matéria de algoritmos e logiga de programação 2
+//incialização das bibliotecas necessarias para a criação do código a ser desenvolvido.
+//@author: Wingly Silva Rodrigues
+//RA: 20038441-5
+
 
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
-#define max 100
+#define MAX 100
 
 //declaração do struct
 typedef struct cadastro
 {
-	char descricao[50];
+	char titulo[50];
+	char autor[30];
+	char editora[30];
 	int quantidade;
-	int categoria;
-	int lote;	
-}CadastroLivro;
+	int ano;
+	int edicao;
+	char isbn[40];
+	int caixa;
+}CadastroObras;
 
+//função com a indentificação
 void indentificacao(){
 	system("cls");
 	int t;
-	for(t=0; t<80; t++) printf("-");
+	for(t = 0; t < 80; t++) printf("-");
 	printf("\n >>>>> WINGLY SILVA RODRIGUES <<<<<");
 	printf("\n >>>>> RA: 20038441-5 <<<<<");
 	printf("\n >>>>> Engenharia de Software <<<<< \n");
-	for(t=0; t<80; t++) printf("-");
+	for(t = 0; t < 80; t++) printf("-");
 }
-//prototipação de funções
 
+//função para cadastrar livros
+
+void CadastrarObras(CadastroObras obras[MAX]){
+	indentificacao();
+	int i;
+	FILE *arq;
+	
+	arq = fopen("arquivo.txt", "a");
+	
+	if(arq != NULL)
+	{
+		for(i = 0; i < 2; i++){
+			printf("\n Informe o Título do livro: ");
+			fflush(stdin);
+			gets(obras[i].titulo);
+			printf("\n Informe o autor do livro: ");
+			fflush(stdin);
+			gets(obras[i].autor);
+			printf("\n Informe a edição:");
+			fflush(stdin);
+			scanf("%d", &obras[i].edicao);
+			printf("\n Digite o ISBN do livro: ");
+			fflush(stdin);
+			gets(obras[i].isbn);
+			
+			
+		}
+	}
+}
 
 //função principal
-
-int main()
-{
-	//Cadastrolivro livro[MAX];
-	setlocale(LC_ALL, "");
-	
-	int op, buscaLote, buscaCategoria;
-//	do{
-		indentificacao();
-		printf("\n1 - Cadastrar Livro");
-		printf("\n2 - Cadastrar Revista");
-		printf("\n3 - Listar todos os produtos");
-		
-	//}
+int main(){
+	setlocale(LC_ALL,"");
+	indentificacao();
+	CadastrarObras("teste");
 }
+
+
+
